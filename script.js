3d3ea5088705dfc40df6de5ee390ee98d02d5b60
Derek <dereklau15@gmail.com>
2023-05-10 20:14:26 +0900

    Add function for changePlayer, create winning combos

--- script.js
+++ script.js
@@ -7,33 +7,59 @@ const Player = function (mark) {
 // module
 
 const gameboard = (() => {
-  let board = Array.from(document.querySelectorAll(".box"));
-  console.log(board);
-  let gameState = ["X", "", "", "", "", "", "", "", ""];
+  const board = Array.from(document.querySelectorAll(".box"));
+  let gameState = ["", "", "", "", "", "", "", "", ""];
+  console.log(gameState);
+
   const playerOne = Player("X");
   const playerTwo = Player("O");
-  let currentPlayer = playerOne;
+  let currentPlayer = playerOne.mark;
 
   function drawMark() {
-    board.forEach((box) => box.addEventListener("click", clickedBox));
+    board.forEach((box) => {
+      box.addEventListener("click", clickedBox);
+    });
   }
 
   function clickedBox(event) {
-    console.log(event.target);
-    const index = event.target.getAttribute("data-index");
-    console.log(index);
-  }
-
-  function populateBoard() {
-    board = gameState;
+    let index = event.target.getAttribute("data-index");
+    if (gameState[index] === "") {
+      gameState[index] = currentPlayer;
+      board[index].innerHTML = currentPlayer;
+    }
+    changePlayer(gameState);
     console.log(board);
+    console.log(gameState);
+    checkWin(gameState);
   }
 
-  drawMark();
-  populateBoard();
+  function changePlayer(stateArray) {
+    let countX = stateArray.filter((mark) => mark === playerOne.mark).length;
+    let countO = stateArray.filter((mark) => mark === playerTwo.mark).length;
+    if (countX > countO) {
+      currentPlayer = playerTwo.mark;
+    } else {
+      currentPlayer = playerOne.mark;
+    }
+  }
 
-  return {
-    playerOne,
-    playerTwo,
-  };
+  function checkWin(gameStateArray) {
+    const winCombos = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [3, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    winCombos.forEach((combo) => {
+      console.log(combo);
+      console.log(combo[0]);
+      console.log(gameStateArray);
+    });
+  }
+  drawMark();
 })();
